31236b28071411ff2f664c8aa1250b74cb02d9e2
VaruneshT <tvarunesharasu@gmail.com>
2025-07-17 21:32:27 +0530

    Updated reset_postgres_password.sql

--- db/reset_postgres_password.sql
+++ db/reset_postgres_password.sql
@@ -1,3 +1,3 @@
 -- This SQL command resets the password for the PostgreSQL user 'postgres' to 'biometric123'.
-
+-- Make sure to run this command with appropriate privileges.
 ALTER USER postgres WITH PASSWORD 'biometric123';
